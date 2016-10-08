f839cc1960b487bf95b3e861d07f4c68116ce765
Thibault Piana <astres3000@free.fr>
2016-10-08 17:52:41 +0200

    New version

--- image.php
+++ image.php
@@ -1,15 +1,17 @@
 <?php
 header ("Content-type: image/png");  // On dit que l'on va crée un png
 
-// putenv('GDFONTPATH=' . realpath('.'));  
+// putenv('GDFONTPATH=' . realpath('.'));
 $repFont = 'fonts/';
 $repImg = "images/";
 $name = "b";
 
 // Recupération des variables
 $font = $repFont.$_GET['font'];
+
 $prenom = ucfirst($_GET['prenom']);
 $nom = ucfirst($_GET['nom']);
+
 $classe = $_GET['classe'];
 
 $cote = $_GET['cote'];
@@ -21,72 +23,72 @@ $longueur_nom = strlen($nom);
 
 switch($longueur_prenom)
 {
-	case ($longueur_prenom <= 7):
-		$size_font_prenom = 31; 
-	break;
-	
-	case '8':
-		$size_font_prenom = 30; 
-	break;
-	
-	case (($longueur_prenom >= 9) && ($longueur_prenom <= 11)):
-		$size_font_prenom = 28; 
-	break;
-	
-	case ($longueur_prenom >= 12):
-		$size_font_prenom = 25;
-	break;
+    case ($longueur_prenom <= 7):
+        $size_font_prenom = 31;
+        break;
+
+    case '8':
+        $size_font_prenom = 30;
+        break;
+
+    case (($longueur_prenom >= 9) && ($longueur_prenom <= 11)):
+        $size_font_prenom = 28;
+        break;
+
+    case ($longueur_prenom >= 12):
+        $size_font_prenom = 25;
+        break;
 }
 switch($longueur_nom)
 {
-	case ($longueur_nom <= 13):
-		$size_font_nom = 24; 
-	break;
-	
-	case ($longueur_nom >= 14):
-		$size_font_nom = 21;
-	break;
+    case ($longueur_nom <= 13):
+        $size_font_nom = 24;
+        break;
+
+    case ($longueur_nom >= 14):
+        $size_font_nom = 21;
+        break;
 }
 switch($sens)
 {
-	case 'h':	
-		if(!empty($classe))
-		{
-			$y_prenom = 81;
-			$y_nom = 128;
-			$y_classe = 185;
-		}
-		else
-		{
-			$y_prenom = 105;
-			$y_nom = 174;
-		}
-		$name = $name."h";
-	break;
-	
-	case 'b':
-		if(!empty($classe))
-		{
-			$y_prenom = 131;
-			$y_nom = 178;
-			$y_classe = 235;
-		}
-		else
-		{
-			$y_prenom = 155;
-			$y_nom = 224;
-		}
-		$name = $name."b";
-	break;
+    case 'h':
+        if(!empty($classe))
+        {
+            $y_prenom = 81;
+            $y_nom = 128;
+            $y_classe = 185;
+        }
+        else
+        {
+            $y_prenom = 105;
+            $y_nom = 174;
+        }
+        $name = $name."h";
+        break;
+
+    case 'b':
+        if(!empty($classe))
+        {
+            $y_prenom = 131;
+            $y_nom = 178;
+            $y_classe = 235;
+        }
+        else
+        {
+            $y_prenom = 155;
+            $y_nom = 224;
+        }
+        $name = $name."b";
+        break;
 }
 switch($cote)
 {
-	case 'g':
-		$name = $name."g";
-	break;
-	case 'd':
-		$name = $name."d";
-	break;
+    case 'g':
+        $name = $name."g";
+        break;
+    case 'd':
+        $name = $name."d";
+        break;
 }
 
 $img = imagecreatefrompng($repImg.$name.".png");
@@ -96,35 +98,35 @@ $img = imagecreatefrompng($repImg.$name.".png");
 $blanc = imagecolorallocate($img, 255, 255, 255);
 
 // ------------------------------------ //
-$taille_prenom = imageftbbox($size_font_prenom, 0, $font, $prenom); // Retourne un array
+$taille_prenom = imageftbbox($size_font_prenom, 0, '/fonts/edosz.ttf', $prenom); // Retourne un array
 $taille_nom = imageftbbox($size_font_nom, 0, $font, $nom); // Retourne un array
 
 if(!empty($classe))
-	$taille_classe = imageftbbox(20, 0, $font, $classe); // Retourne un array
+    $taille_classe = imageftbbox(20, 0, $font, $classe); // Retourne un array
 
 $infos_prenom['largeur'] = $taille_prenom[4];
 $infos_nom['largeur'] = $taille_nom[4];
 $infos_image['largeur'] = imagesx($img);
 if(!empty($classe))
-	$infos_classe['largeur'] = $taille_classe[4];
+    $infos_classe['largeur'] = $taille_classe[4];
 // ----------------------------------- //
 
 $x_prenom = ($infos_image['largeur'] - $infos_prenom['largeur']) / 2;
 $x_nom = ($infos_image['largeur'] - $infos_nom['largeur']) / 2;
 
 if(!empty($classe))
-	$x_classe = ($infos_image['largeur'] - $infos_classe['largeur']) / 2;
+    $x_classe = ($infos_image['largeur'] - $infos_classe['largeur']) / 2;
 
 if(empty($classe))
 {
-	imagettftext($img, $size_font_prenom, 0, $x_prenom, $y_prenom, $blanc, $font, $prenom);
-	imagettftext($img, $size_font_nom, 0, $x_nom, $y_nom, $blanc, $font, $nom);
+    imagettftext($img, $size_font_prenom, 0, $x_prenom, $y_prenom, $blanc, $font, $prenom);
+    imagettftext($img, $size_font_nom, 0, $x_nom, $y_nom, $blanc, $font, $nom);
 }
 else
 {
-	imagettftext($img, $size_font_prenom, 0, $x_prenom, $y_prenom, $blanc, $font, $prenom);
-	imagettftext($img, $size_font_nom, 0, $x_nom, $y_nom, $blanc, $font, $nom);
-	imagettftext($img, 20, 0, $x_classe, $y_classe, $blanc, $font, $classe);
+    imagettftext($img, $size_font_prenom, 0, $x_prenom, $y_prenom, $blanc, $font, $prenom);
+    imagettftext($img, $size_font_nom, 0, $x_nom, $y_nom, $blanc, $font, $nom);
+    imagettftext($img, 20, 0, $x_classe, $y_classe, $blanc, $font, $classe);
 }
 
 imagealphablending($img, false);
